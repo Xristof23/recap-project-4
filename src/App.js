7328f6ea30b406b034e876bb8383b5c3b9b7a784
Mariia Posselt <maruw@ya.ru>
2024-02-15 11:31:23 +0100

    add submit handling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
+import { useState } from "react";
 import "./App.css";
 import Form from "./components/Form/Form";
+import { uid } from "uid";
+
+const initialActivities = [
+  { id: 1, name: "Walking", isForGoodWeather: true },
+  { id: 2, name: "Watch television", isForGoodWeather: false },
+];
 
 function App() {
+  const [activities, setActivities] = useState(initialActivities);
+
+  function handleActivity(name, isForGoodWeather) {
+    setActivities([
+      ...activities,
+      {
+        id: uid(),
+        name: name,
+        isForGoodWeather: isForGoodWeather === "on" ? true : false,
+      },
+    ]);
+  }
+
+  //console.log(activities);
+
   return (
     <div className="App">
       <h1>Super Weather App 3000</h1>
-      <Form />
+      <Form onAddActivity={handleActivity} />
     </div>
   );
 }
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,17 +1,27 @@
 export default function Form({ onAddActivity }) {
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+
+    onAddActivity(data.name, data.isForGoodWeather);
+    //console.log(data.isForGoodWeather);
+    event.target.reset();
+    event.target.elements.name.focus();
+  }
+
   return (
-    <form>
-      <h2>Activity Form</h2>
+    <form onSubmit={handleSubmit}>
+      <h2>Add new activity</h2>
       <label htmlFor="name">
-        Name of activity <input name="name" type="text"></input>
+        Name of activity: <input name="name" type="text" required></input>
       </label>
-      <label htmlFor="check">
-        Weather activity (good or bad)
-        <input name="check" type="checkbox"></input>
+      <br />
+      <label htmlFor="isForGoodWeather">
+        Good-weather activity:
+        <input name="isForGoodWeather" type="checkbox" />
       </label>
-      <button onClick={onAddActivity} type="submit">
-        Submit
-      </button>
+      <button type="submit">Submit</button>
     </form>
   );
 }
